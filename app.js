e8e8edfe4ee2e006d02699f3c058ba0a80e630b3
Narky <narky05@gmail.com>
2016-05-12 15:30:47 +0800

    处理逻辑更改，直接输出图片

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import querystring from 'querystring'
 import shell from 'shelljs'
 import fs from 'fs'
 import path from 'path'
+import base64Img from "base64-img"
 
 const app = new Koa()
 const router = new Router()
@@ -12,7 +13,7 @@ app.use(async (ctx, next) => {
     try {
         await next()
     } catch (err) {
-        ctx.body = err
+        ctx.body = jsonParse({'code': -1, 'error': err, 'msg': ''})
         ctx.status = err.status || 500
     }
 })
@@ -24,39 +25,30 @@ app.use(async (ctx, next) => {
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-const doShell = function (cmd) {
-    return new Promise((resolve, reject) => {
-        let res = shell.exec(cmd)
-        if (res.code == 0) resolve(res.code)
-        else reject(-1)
-    })
-}
-
-const jsonParse = function (obj) {
-    return JSON.stringify(obj)
-}
-
 router
     .get('/', async (ctx, next) => {
         ctx.body = 'Welcome to Website Thumbler'
     })
-    // .param('url', async(url, next) => {
-    //     console.log(ctx.params);
-    //     await next()
-    // })
     .get('/capture*', async (ctx, next) => {
+        let params = querystring.parse(ctx.url.replace(/^\/capture(\/)?\?/, ''))
         let root = __dirname
         let picDir = path.join(root, 'screenshot')
-        if(!fs.existsSync(picDir)){
-            fs.mkdirSync(picDir)
+        if (params.docker && params.docker == 1) {
+            picDir = '/screenshot'
+        } else {
+            if(!fs.existsSync(picDir)){
+                fs.mkdirSync(picDir)
+            }
         }
-        let params = querystring.parse(ctx.url.replace(/\/capture(\/)?\?/, ''))
-        let cmd = [path.join(root, 'bin/phantomjs'), path.join(root, 'rasterize.js'), params.url, path.join(picDir, params.name)].join(' ')
-        //console.log(cmd)
-        //ctx.body = 'RUN:' + cmd
-        let code = await doShell(cmd)
-        ctx.body = jsonParse({'code': code, 'msg': ''})
+        console.log("==PictureDir==", picDir)
+        let picPath = path.join(picDir, params.name+'.png')
+        let cmd = [path.join(root, 'bin/phantomjs'), path.join(root, 'rasterize.js'), params.url, picPath].join(' ')
+        console.log('==Run CMD==', cmd)
+        let code = shell.exec(cmd).code;
+        let dataBuffer = new Buffer(base64Img.base64Sync(picPath).replace(/^data:image\/\w+;base64,/, ""), 'base64')
+        ctx.body = dataBuffer
+        ctx.type = 'image/png'
     })
 
 app.use(router.routes());
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
