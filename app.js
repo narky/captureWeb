235b88c08927c6781d631c916c17e6ebe23f842a
HangXuan <xuan.hang@mi-me.com>
2016-08-31 16:25:08 +0800

    引入phantomjs-prebuilt，自动加载phantomjs，一些代码的优化

--- app.js
+++ app.js
@@ -1,58 +1,67 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import querystring from 'querystring'
+import phantomjs from 'phantomjs-prebuilt'
 import shell from 'shelljs'
 import fs from 'fs'
 import path from 'path'
 import base64Img from "base64-img"
+import chalk from "chalk"
 
 const app = new Koa()
 const router = new Router()
 
-const jsonParse = function (obj) {
-    return JSON.stringify(obj)
+const getFileName = () => {
+  let d = new Date()
+  return [d.getFullYear(), d.getMonth()+1, d.getDate(), ~~(Math.random() * 1000)].join('')
 }
 
-app.use(async (ctx, next) => {
-    try {
-        await next()
-    } catch (err) {
-        ctx.body = jsonParse({'code': -1, 'error': err, 'msg': ''})
-        ctx.status = err.status || 500
-    }
+app.use(async(ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.body = JSON.stringify({
+      'code': -1,
+      'error': err,
+      'msg': ''
+    })
+    ctx.status = err.status || 500
+  }
 })
 
-app.use(async (ctx, next) => {
-    const start = new Date()
-    await next()
-    const ms = new Date() - start
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+app.use(async(ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 router
-    .get('/', async (ctx, next) => {
-        ctx.body = 'Welcome to Website Thumbler'
-    })
-    .get('/capture*', async (ctx, next) => {
-        let params = querystring.parse(ctx.url.replace(/^\/capture(\/)?\?/, ''))
-        let root = __dirname
-        let picDir = path.join(root, 'screenshot')
-        if (params.docker && params.docker == 1) {
-            picDir = '/screenshot'
-        } else {
-            if(!fs.existsSync(picDir)){
-                fs.mkdirSync(picDir)
-            }
-        }
-        console.log("==PictureDir==", picDir)
-        let picPath = path.join(picDir, params.name+'.png')
-        let cmd = [path.join(root, 'bin/phantomjs'), path.join(root, 'rasterize.js'), params.url, picPath].join(' ')
-        console.log('==Run CMD==', cmd)
-        let code = shell.exec(cmd).code;
-        let dataBuffer = new Buffer(base64Img.base64Sync(picPath).replace(/^data:image\/\w+;base64,/, ""), 'base64')
-        ctx.body = dataBuffer
-        ctx.type = 'image/png'
-    })
+  .get('/', async(ctx, next) => {
+    ctx.body = 'Welcome to Website Thumbler'
+    // ctx.body = os.platform()
+  })
+  .get('/capture*', async(ctx, next) => {
+    let params = querystring.parse(ctx.url.replace(/^\/capture(\/)?\?/, ''))
+    let root = __dirname
+    let picDir = path.join(root, 'screenshot')
+    if (params.docker && params.docker == 1) {
+      picDir = '/screenshot'
+    } else {
+      if (!fs.existsSync(picDir)) {
+        fs.mkdirSync(picDir)
+      }
+    }
+    console.log("[" + chalk.blue('PictureDir') + "] ", picDir)
+    let fileName = (!params.name || params.name === '') ? getFileName() : params.name
+    let picPath = path.join(picDir, fileName + '.png')
+    let cmd = [phantomjs.path, path.join(root, 'rasterize.js'), params.url, picPath].join(' ')
+    console.log("["+chalk.red('CMD')+"] ", cmd)
+    let code = shell.exec(cmd).code;
+    let dataBuffer = new Buffer(base64Img.base64Sync(picPath).replace(/^data:image\/\w+;base64,/, ""), 'base64')
+    ctx.body = dataBuffer
+    ctx.type = 'image/png'
+  })
 
-app.use(router.routes());
-app.listen(process.env.PORT || 8000);
+app.use(router.routes())
+app.listen(process.env.PORT || 8000)
--- start.js
+++ start.js
@@ -2,8 +2,8 @@ require("babel-core/register")(
     {
         presets: ['stage-3','es2015']
     }
-);
+)
 
-require("babel-polyfill");
+require("babel-polyfill")
 
-require("./app.js");
+require("./app.js")
